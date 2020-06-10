80ff6cddc726507b3dca6201c91253913a706ad7
denis909 <denis909@mail.ru>
2020-06-10 13:43:36 +0300

    n/a

--- MainLayout.php
+++ MainLayout.php
@@ -4,4 +4,29 @@ namespace backend\theme;
 
 class MainLayout extends \yii\base\Widget
 {
+
+    public $copyright;
+
+    public $optionsMenu = [];
+
+    public $content;
+
+    public $breadcrumbs = [];
+
+    public $user;
+
+    public $infoMessages = [];
+
+    public $errorMessages = [];
+
+    public $successMessages = [];
+
+    public $actionMenu = [];
+
+    public $mainMenu = [];
+
+    public $enableCard = true;
+
+    public $cardTitle;
+
 }
